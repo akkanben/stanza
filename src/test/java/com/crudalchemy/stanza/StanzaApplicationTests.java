467acd06af071a7b7acd420fa212aef901f74db3
Ben Mills <fuzzyllama@gmail.com>
2022-03-14 09:58:21 -0700

    Adds intial spring intializr setup and basic package structure

---
+++ src/test/java/com/crudalchemy/stanza/StanzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudalchemy.stanza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StanzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
