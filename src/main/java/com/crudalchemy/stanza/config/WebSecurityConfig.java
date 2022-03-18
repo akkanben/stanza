d222d979d2db1723db9bf022a138ccbc332e6ab5
Robb Alexander <robbmalexander@gmail.com>
2022-03-18 01:17:24 -0700

    styled general thread page, font selections, admin badge adjustment

--- src/main/java/com/crudalchemy/stanza/config/WebSecurityConfig.java
+++ src/main/java/com/crudalchemy/stanza/config/WebSecurityConfig.java
@@ -37,6 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login").permitAll()
                 .antMatchers("/images/**").permitAll()
                 .antMatchers("/css/**").permitAll()
+                .antMatchers("/profile/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
